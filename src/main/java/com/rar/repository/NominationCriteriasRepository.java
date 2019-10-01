173c45605dc296a36aef92de73e82b728d0149af
anjali <anjali.garg@nineleaps.com>
2019-10-01 15:02:07 +0530

    Extra nominatiion_criteria removed

--- src/main/java/com/rar/repository/NominationCriteriasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rar.repository;//package com.signon.repository;
-//
-//import com.signon.model.NominationCriterias;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface NominationCriteriasRepository extends CrudRepository<NominationCriterias,Long> {
-//}
