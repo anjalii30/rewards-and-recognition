94c2bd6b089e646f0a7d47ae1eb4db4e0d0a67fb
anjali <anjali.garg@nineleaps.com>
2019-10-03 16:31:25 +0530

    post mapping for list assigned users

--- src/main/java/com/rar/controller/ProjectController.java
+++ src/main/java/com/rar/controller/ProjectController.java
@@ -39,7 +39,7 @@ public class ProjectController {
         return  projectService.findAllData();
     }
 
-    @GetMapping("/listAssignedUsers")
+    @PostMapping("/listAssignedUsers")
     public List UsersForProject(@RequestHeader(value = "Authorization") String token,@RequestBody Projects project_name) throws Exception {
         String email=validity.check(token);
         Long project_id = projectService.getIdByProject(project_name.getProject_name());
