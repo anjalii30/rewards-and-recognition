2d273ecf11ff0230ac66e7bc6e763f0900b7e506
anjali <anjali.garg@nineleaps.com>
2019-10-09 14:22:19 +0530

    token along with awarded

--- src/main/java/com/rar/controller/AwardedController.java
+++ src/main/java/com/rar/controller/AwardedController.java
@@ -47,8 +47,8 @@ public class AwardedController {
 
     @ApiOperation(value = "show the awarded list")
     @GetMapping("/awardedList")
-    public List awardedList(/*@RequestHeader(value = "Authorization") String token*/){
-      //  String email=validity.check(token);
+    public List awardedList(@RequestHeader(value = "Authorization") String token){
+        String email=validity.check(token);
         return  awardedService.findAllAwarded();
     }
 
