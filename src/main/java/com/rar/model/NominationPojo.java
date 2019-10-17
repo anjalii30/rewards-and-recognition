db3254a42496b40d3763737904d31b6be48f970c
RamaKrishnaRaju <krishna.raju@nineleaps.com>
2019-10-17 12:27:20 +0530

    Changes in Nominations

--- src/main/java/com/rar/model/NominationPojo.java
+++ src/main/java/com/rar/model/NominationPojo.java
@@ -3,7 +3,6 @@ package com.rar.model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-import java.util.Date;
 import java.util.List;
 
 @ApiModel(description = "All the DTO data in nominations")
@@ -14,12 +13,6 @@ public class NominationPojo {
     private String project_name;
     @ApiModelProperty(notes = "The ID of the particular user")
     private long userId;
-    @ApiModelProperty(notes = "The frequency of the reward")
-    private String frequency;
-    @ApiModelProperty(notes = "The starting date of the Project")
-    private Date start_date;
-    @ApiModelProperty(notes = "The ending date of the Project")
-    private Date end_date;
     @ApiModelProperty(notes = "The name of the employee for that particular nomination")
     private String employee_name;
     @ApiModelProperty(notes = "Used in self nominated rewards when a manager approves it")
@@ -28,6 +21,8 @@ public class NominationPojo {
     private boolean disable=false;
     @ApiModelProperty(notes = "The name of the particular reward")
     private String reward_name;
+    @ApiModelProperty(notes = "The reason for nomination")
+    private String reason;
     @ApiModelProperty(notes = "The list of evidences for a particular nomination")
     private List<EvidencesPojo> evidencesPojoList;
 
@@ -51,6 +46,14 @@ public class NominationPojo {
         return project_name;
     }
 
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
     public void setProject_name(String project_name) {
         this.project_name = project_name;
     }
@@ -63,9 +66,6 @@ public class NominationPojo {
         this.userId = userId;
     }
 
-    public String getFrequency() {
-        return frequency;
-    }
 
     public String getReward_name() {
         return reward_name;
@@ -73,26 +73,6 @@ public class NominationPojo {
 
     public void setReward_name(String reward_name) {
         this.reward_name = reward_name;
-    }
-
-    public void setFrequency(String frequency) {
-        this.frequency = frequency;
-    }
-
-    public Date getStart_date() {
-        return start_date;
-    }
-
-    public void setStart_date(Date start_date) {
-        this.start_date = start_date;
-    }
-
-    public Date getEnd_date() {
-        return end_date;
-    }
-
-    public void setEnd_date(Date end_date) {
-        this.end_date = end_date;
     }
 
     public boolean isSelected() {
