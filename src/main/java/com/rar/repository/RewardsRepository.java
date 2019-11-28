b84c4db7cf131929bc529595b28312c246965be7
Shikhar <shikhar.raizada@nineleaps.com>
2019-11-28 13:37:17 +0530

    latest reward changed

--- src/main/java/com/rar/repository/RewardsRepository.java
+++ src/main/java/com/rar/repository/RewardsRepository.java
@@ -26,7 +26,7 @@ public interface RewardsRepository extends CrudRepository<Rewards, Long> {
     @Query(value = "Update rewards set regenerated=false where reward_id=?1", nativeQuery = true)
     void updateToNull(long id);
 
-    @Query(value = " Select * from rewards where award_status=1  and reward_id not in(select reward_id from nominations where user_id=?1) Order by start_date DESC limit 6",nativeQuery = true)
+    @Query(value = " Select * from rewards where award_status=1 and self_nominate=1  and reward_id not in(select reward_id from nominations where user_id=?1) Order by start_date DESC limit 6",nativeQuery = true)
     List<Rewards> latest(Long user_id);
 
    @Query(value="select * from rewards where award_status=1 and self_nominate=true and reward_id not in(select reward_id from nominations where selected=true)",nativeQuery = true)
