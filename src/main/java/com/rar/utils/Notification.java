27a766fd22ed8016cec44bc6f13926534be8f937
anjali <anjali.garg@nineleaps.com>
2019-12-12 19:23:42 +0530

    web socket

---
+++ src/main/java/com/rar/utils/Notification.java
@@ -0,0 +1,15 @@
+package com.rar.utils;
+
+public class Notification {
+
+    private String message;
+
+    public Notification (String content) {
+        this.message = content;
+    }
+
+    public String getContent() {
+        return message;
+    }
+
+}
