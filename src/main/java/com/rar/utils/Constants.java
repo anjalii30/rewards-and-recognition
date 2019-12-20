5e850cdb702ac853e84d8f06fe732e7b635918c4
anjali <anjali.garg@nineleaps.com>
2019-12-20 17:27:16 +0530

    jwt config

--- src/main/java/com/rar/utils/Constants.java
+++ src/main/java/com/rar/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.rar.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public final class Constants {
 
     public final static Integer CREATED = 0;
@@ -12,5 +14,8 @@ public final class Constants {
 
     public final static Integer ROLE_EMPLOYEE = 1;
     public final static Integer ROLE_ADMIN = 2;
+    
+    @Value("${jwt.secret}")
+    public final static String secret = "xsNxAnioE69S0DZ4YUVUrCzR";
 
 }
