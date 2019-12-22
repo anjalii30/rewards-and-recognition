e4d56f1ef0a5fa2981501323c791608ff414c624
RamaKrishnaRaju <krishna.raju@nineleaps.com>
2019-12-22 14:39:47 +0530

    KrishnaRaju | Evidence id type changed

--- src/main/java/com/rar/model/Evidences.java
+++ src/main/java/com/rar/model/Evidences.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 public class Evidences {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "evidence_id")
     @ApiModelProperty(notes = "The database generated evidenceID")
     private Long evidenceID;
